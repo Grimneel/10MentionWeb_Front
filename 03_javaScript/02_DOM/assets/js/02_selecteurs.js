d060a0fd7840ac230c030e9ca20f05d500ba30de
FabienP <fabienpottier@hotmail.fr>
2024-03-28 16:49:53 +0100

    02_les sélecteurs en JS

--- 03_javaScript/02_DOM/assets/js/02_selecteurs.js
+++ 03_javaScript/02_DOM/assets/js/02_selecteurs.js
@@ -68,10 +68,122 @@ for (let i = 0; i < lesP.length; i++) {
 }
 
 // 3ème façon de faire
+
+// Boucle for of : je déclare une variable p qui représente chaque élément dans le tableau lesP, au niveau de l'instruction à exécuter j'appelle la variable p en lui attribuant le style souhaité avec la propriété style.
+
 for ( let p of lesP ) {
 
     p.style.textTransform = "uppercase";
 
 }
 
-// Boucle for of : je déclare une variable p qui représente chaque élément dans le tableau lesP, au niveau de l'instruction à exécuter j'appelle la variable p en lui attribuant le style souhaité avec la propriété style.
+
+// title[0].style.backgroundColor = "pink";
+// title[0].style.textAlign = "center";
+
+// Avec une classe pink dans le titre h1
+
+let pink = document.getElementsByClassName('pink');
+
+pink[0].style.backgroundColor = "pink";
+pink[0].style.textAlign = "center";
+
+
+// Sélection via queryselecteur ( sélectionne le premier élément trouvé)
+
+let div = document.querySelector('div');
+// console.log(div);
+
+div.style.border = "1px solid #000";
+
+// La méthode querySelector() retourne le 1er élément trouvé spécifié en paramètres.
+// L'avantage de querySelector c'est qu'il utilise la syntaxe de sélection CSS -> #monId .maClass <balise>.
+
+// Sélection via querySelectorAll() (selectionne tous les éléments trouvés)
+// sélectionne toutes les balises en ensemble et retourne un array d'éléments
+
+let query = document.querySelectorAll('.selectClass');
+
+for (let i = 0; i < query.length; i++) {
+    
+    console.log(query[i].innerText) // on a 3 paragraphe qui ont la classe selectClass
+}
+
+//La propriété innerText est similaire à innerHTML, mais elle fonctionne avec le texte brut à l'intérieur d'un élément, plutôt qu'avec le HTML. Elle permet d'accéder au texte contenu dans un élément HTML et de le modifier si nécessaire.
+
+let rajout = document.querySelector(".bg-success p");
+rajout.innerText = "Je suis là";
+
+
+// CRÉATION D'UN NOUVEL ÉLÉMENT 
+let section = document.querySelector('section');
+// Pour créer un élément, nous utilisons la méthode createElement (), puis nous l'intégrons dans le noeud souhaité
+let nouveauParaFin = document.createElement('p');
+
+nouveauParaFin.innerHTML = "<strong> Coucou, je suis ton nouveau paragraphe à la fin de la section </strong>";
+nouveauParaFin.style.color = "red";
+
+section.append(nouveauParaFin); // append() -> insère du contenu à la fin de la section.
+// append() accepte tous les éléments (balise ou string).
+
+let nouveauParaDebut = document.createElement('p');
+nouveauParaDebut.innerHTML = "<strong> Coucou, je suis ton nouveau paragraphe au début de la section </strong>";
+section.prepend(nouveauParaDebut);// prepend() -> insére du contenu au début de la section
+nouveauParaDebut.style.color = "green"; 
+
+// DÉPLACER UN ÉLÉMENT
+
+// Pour déplacer un élément, il faut trois paramètres
+/**
+ *  -> le parent qui va accueillir l'élément
+ *  -> l'élément à déplacer
+ *  -> l'élément qui vient après
+*/
+
+let parent = document.querySelector('main');
+let jeBouge = document.querySelector('h4');
+let h2 = document.querySelector('subtitle');
+
+parent.insertBefore(jeBouge, h2);
+
+
+// SUPPRIMER UN ÉLÉMENT
+
+// Pour supprimer un élément, il faut deux paramètres :
+/**
+ *  -> le parent 
+ *  -> l'élément à supprimer
+*/
+
+let ul = document.querySelector('ul');
+
+let li = document.querySelector('ul :nth-child(2)');
+console.log(li);
+
+// Pour supprimer, on utilise .removeChild()
+ul.removeChild(li);
+
+// CRÉER UN ATTRIBUT ET SA VALEUR
+// setAttribute() : méthode pour ajouter un nouvel attribut ou changer la valeur d'un attribut existant pour un élément
+
+let baliseA = document.querySelector('a');
+baliseA.setAttribute("attribut", "valeurAttribut"); // je rajoute un attribut à la balise a
+
+baliseA.setAttribute("href", "01_introduction.html"); // changer la valeur de l'attribut href
+
+let lesA = document.querySelectorAll('a');
+
+let valeurA = lesA[3].getAttribute('href'); // ici je récupère la valeur de l'attribut href avec le getAttribute()
+console.log(valeurA);
+
+// Grâce à votre script, créez une balise a dans la balise h1 avec le lien vers la documentation JS () qui s'affiche sur un nouvel onglet.
+// Ce lien sera de couleur blanche et non souligné.
+
+
+let nouveauA = document.querySelector('h1');
+nouveauA = document.createElement('a');
+nouveauA = document.setAttribute("href");
+
+
+
+
