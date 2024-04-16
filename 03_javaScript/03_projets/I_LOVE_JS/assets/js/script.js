b3356703327d0c5da84744c0dc6c340be578e704
FabienP <fabienpottier@hotmail.fr>
2024-04-16 16:54:00 +0200

    suite i love js

--- 03_javaScript/03_projets/I_LOVE_JS/assets/js/script.js
+++ 03_javaScript/03_projets/I_LOVE_JS/assets/js/script.js
@@ -103,3 +103,74 @@ btnCookies.addEventListener("click", ()=> {
 
 
 })
+
+// -------------------------- Carrousel -------------------------- //
+
+let left = document.querySelector('.left');
+let automatic = document.querySelector('.automatic');
+let right = document.querySelector('.right')
+let img = document.querySelector('img');
+let automaticIcon = document.querySelector('.automatic i');
+
+let i = 1;
+
+
+right.addEventListener('click', carrousel);
+    
+    function carrousel(){
+
+    i++; // on incrémente puis on affecte : la valeur de la variable sera donc 1 / puis 2 / puis 3 / etc.
+    img.setAttribute('src', `assets/img/${i}.jpg`);
+    
+    if (i == 7) {
+        i = 1;
+        img.setAttribute('src', `assets/img/${i}.jpg`);
+        // j'appelle de façon automatique les images par leur nom
+    }
+
+};
+
+left.addEventListener('click', ()=> {
+    
+    i--;
+    img.setAttribute('src', `assets/img/${i}.jpg`);
+    
+    if (i == 0) {
+         i= 6;
+        img.setAttribute('src', `assets/img/${i}.jpg`);
+    }
+
+})
+
+let statut = null; // Initialisation d'une variale pour stocker l'état du slider (lecture ou pause)
+automatic.addEventListener('click', ()=> {
+    // Inversion de la classe 'bi-pause-fill' sur l'élément automaticIcon à chaque clic
+    automaticIcon.classList.toggle('bi-pause-fill');
+
+    // Vérification de l'état actuel du slider
+    if (statut == null) {
+        // Si le slider est en pause
+        // Activation du mode lecture : exécution de la fonction carrousel toutes les 1500 millisecondes (1.5 secondes)
+
+        statut = window.setInterval(carrousel, 1500); //setInterval est utilisé pour exécuter une fonction à intervalles réguliers
+
+    } else {
+        // Si le slider est en lecture
+        // Désactivation du mode lecture : arrêt de l'exécution de la fonction carrousel à intervalles réguliers
+        window.clearInterval(statut);
+
+        //clearInterval est utilisé pour arrêter l'exécution de cette fonction à intervalles réguliers en utilisant l'identifiant de l'intervalle retourné par setInterval.
+        
+        statut = null; // Réinitialisation de la variable statut à null pour indiquer que le slider est en pause
+
+    }
+})
+
+
+
+// -------------------------- Loader -------------------------- //
+
+let loader = document.querySelector('#loader');
+window.addEventListener('load', ()=>{
+    loader.classList.add('hideLoader');
+})
