18154ac84db4149ca85e5d836ca4c339f2c08f6c
FabienP <fabienpottier@hotmail.fr>
2024-05-03 16:53:22 +0200

    ajout à la maquette intégration

---
+++ 00_maquettes/maquette_integration/assets/js/script.js
@@ -0,0 +1,59 @@
+// ------------- Test lien HTML/JS ------------------- //
+/* The `alert('Bonjour !!!!!!!!!!!!')` statement is displaying a pop-up dialog box with the message
+"Bonjour !!!!!!!!!!!!". This is a common way to provide a simple message or notification to the user
+when a webpage is loaded. */
+// alert('Bonjour !!!!!!!!!!!!')
+
+
+
+//------------------  Vérification email ------------- //
+
+let form = document.querySelector('#form');
+let email = document.querySelector('#email');
+let sujet = document.querySelector('#subject')
+let myAlert = document.querySelector('#alert');
+
+
+function setValidation(element, infos = null) {
+
+    let formControl = element.parentElement;
+
+    let small = formControl.querySelector('small');
+    if (infos != null) {
+        small.innerText = infos;
+        formControl.className = "form-control error";
+    } else {
+        formControl.className = "form-control success";
+    }
+}
+
+
+form.addEventListener('submit', (event) => {
+
+        event.preventDefault(); // Une méthode utilisée dans les événements JavaScript pour empêcher le comportement par défaut associé à un évenement de se produire.
+
+        let emailValue = email.value.trim();
+        if (emailValue.includes("")) {
+
+            myAlert.innerHTML = '<p class="alert alert-danger text-center mt-3" role="alert">Veuillez renseigner tout les champs</p>';       
+            
+            for (let input of inputs) {
+                  setValidation(input, message);
+               }
+    
+        } else {
+            myAlert.innerHTML = '';
+
+            let regexEmail = /^(?=.*[a-zA-Z0-9]).{20,200}$/;
+
+                    if (!regexEmail.test(emailValue)) {
+                    
+                        message = 'Email n\est pas valide';
+                        setValidation(email, message);
+                    
+                    } else {
+                        setValidation(email);
+                    }
+        }
+})
+
