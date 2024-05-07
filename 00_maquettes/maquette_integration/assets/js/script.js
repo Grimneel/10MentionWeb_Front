ac94dd1d9e4326916c013eb0e8b4913033b43c0a
FabienP <fabienpottier@hotmail.fr>
2024-05-07 16:45:51 +0200

    fin maquettes

--- 00_maquettes/maquette_integration/assets/js/script.js
+++ 00_maquettes/maquette_integration/assets/js/script.js
@@ -9,11 +9,15 @@ when a webpage is loaded. */
 //------------------  Vérification email ------------- //
 
 let form = document.querySelector('#form');
-let email = document.querySelector('#email');
-let sujet = document.querySelector('#subject')
+let inputEmail = document.getElementById('email');
+let sujet = document.querySelector('#subject');
+let messageTextArea = document.querySelector('#message');
 let myAlert = document.querySelector('#alert');
-
-
+let emailRegExp = /^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$/;
+let sujetRegExp = /^[a-z]{2,20}$/;
+let messageRegExp =/^[a-zA-Z0-9.-_-@]{20,200}$/;
+let inputs = document.querySelectorAll('input');
+let message = "";
 function setValidation(element, infos = null) {
 
     let formControl = element.parentElement;
@@ -26,62 +30,80 @@ function setValidation(element, infos = null) {
         formControl.className = "form-control success";
     }
 }
+    form.addEventListener('submit', (event) => {
+                event.preventDefault(); // Une méthode utilisée dans les événements JavaScript pour empêcher le comportement par défaut associé à un évenement de seproduire.
+        
+                let emailValue = email.value.trim();
+                let sujetValue = sujet.value.trim();
+                let messageValue = messageTextArea.value.trim();
+                let formArray = [emailValue, sujetValue, messageValue];
 
 
-form.addEventListener('submit', (event) => {
-
-        event.preventDefault(); // Une méthode utilisée dans les événements JavaScript pour empêcher le comportement par défaut associé à un évenement de se produire.
-
-        let emailValue = email.value.trim();
-        if (emailValue.includes("")) {
-
-            myAlert.innerHTML = '<p class="alert alert-danger text-center mt-3 d-flex flex-column justify-content-center " role="alert">Veuillez renseigner tout les champs</p>';       
-            
-            for (let input of inputs) {
-                  setValidation(input, message);
-               }
-
-        } else {
-            myAlert.innerHTML = '';
-
-            let regexEmail = /^(?=.*[a-zA-Z0-9]).{20,200}$/;
-
-                    if (!regexEmail.test(emailValue)) {
-
+                if (formArray.includes("")) {
+        
+                    myAlert.innerHTML = '<p class="alert alert-danger text-center mt-3" role="alert">Veuillez renseigner tout les champs</p>';       
+        
+                    for (let input of inputs) {
+                          setValidation(input, message);
+                       }
+        
+                } else {
+                
+                    if (!emailRegExp.test(emailValue)) {
                         message = 'Email n\est pas valide';
                         setValidation(email, message);
-                    
                     } else {
-                        setValidation(email);
+                        setValidation(email); 
                     }
-        }
 
-        let sujetValue = sujet.value.trim();
-        if (sujetValue.includes("")) {
-
-            myAlert.innerHTML = '<p class="alert alert-danger text-center mt-3" role="alert">Veuillez renseigner tout les champs</p>';       
-
-            for (let input of inputs) {
-                  setValidation(input, message);
-               }
-
-        } else {
-            myAlert.innerHTML = '';
-
-            let regexSujet = /^(?=.*[a-zA-Z0-9]).{20,200}$/;
-
-                    if (!regexSujet.test(sujetValue)) {
-                    
-                        message = 'Sujet n\est pas valide';
-                        setValidation(sujet, message);
-                    
+                    if (!sujetRegExp.test(sujetValue)) {
+                            
+                     message = 'Sujet n\'est pas valide';
+                     setValidation(sujet, message);
+                            
                     } else {
-                        setValidation(sujet);
-                    }
-        }
-})
+                    setValidation(sujet);
+                            }
+                    if (!messageRegExp.test(messageValue)) {
+                            
+                        message = 'Sujet n\'est pas valide';
+                        setValidation(messageTextArea, message);
+                                       
+                    } else {
+                               setValidation(messageTextArea);
+                                       }      
+                   
+                }
+                if(myAlert.innerHTML == "" && message == "")
+                    {
+                        myAlert.innerHTML = '<p class="alert alert-danger text-center mt-3" role="alert"><a  href="#">Connected</a></p>';
+                    }  
+        })
+    
 
-setValidation(element, infos);
+        
+// form.addEventListener('submit', function(formError){
+
+//     let emailRegExp = new RegExp('^[a-zA-Z-0-9.-_]+[@]{1}[a-zA-Z-0-9.-_]+[.]{1}[a-z]{2,10}$', 'g');
+
+//     if (inputEmail.ariaValueMax.trim() == ""){
+//         let myError = document.getElementById('error')
+//         myError.innerHTML= "Le champs email est requis";
+//         myError.style.color = 'red';
+//         formError.preventDefault();
+//     } else if (emailRegExp.test(inputEmail.value) == false){
+//         let myError = document.getElementById('error')
+//         myError.innerHTML= "L'email doit comporter un @";
+//         myError.style.color = 'red';
+//         formError.preventDefault();
+//     }
+// })
+
+
+
+// 
+
+
 
 
 
